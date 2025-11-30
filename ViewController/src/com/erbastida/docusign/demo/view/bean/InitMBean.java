8682ca042b1191604b22607cbef9c76f1d344c42
Erick Bastida <erbastida@gmail.com>
2025-11-30 18:28:12 -0500

    Init page created

---
+++ ViewController/src/com/erbastida/docusign/demo/view/bean/InitMBean.java
@@ -0,0 +1,11 @@
+package com.erbastida.docusign.demo.view.bean;
+
+public class InitMBean {
+    public InitMBean() {
+    }
+
+    public String init_button_action() {
+        System.out.println("init_button_action - init");
+        return null;
+    }
+}
